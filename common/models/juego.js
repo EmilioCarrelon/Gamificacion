a4e388773e83aa2987e1527056b674a63392f925
Emilio <emiliocarrelon99@gmail.com>
2018-10-08 09:49:18 +0200

    Metodo remoto getNombre de juego

--- common/models/juego.js
+++ common/models/juego.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function(Juego) {
+/**
+ * Devuelve el nombre del juego
+ * @param {Function(Error, string)} callback
+ */
+
+Juego.prototype.getNombre = function(callback) {
+  var Nombre;
+  // TODO
+  callback(null, this.nombre);
+};
+
 
 };
